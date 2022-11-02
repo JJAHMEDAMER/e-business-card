9a057df4b266e3016e706533b94a394309dfb807
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-02 11:37:05 +0200

    hero photo content

---
+++ src/comp/hero-photo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Images
+import heroPhoto from "../assets/Image.png";
+
+
+
+const HeroPhoto = () => (
+  <div>
+    <img src={heroPhoto} />
+  </div>
+);
+
+export default HeroPhoto;
