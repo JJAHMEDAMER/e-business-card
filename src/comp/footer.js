1959e6a74ac5c032155161ff75d8a2438100fb8a
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-02 11:36:52 +0200

    footer content

---
+++ src/comp/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Images
+import twitter from "../assets/Twitter Icon.png";
+import instagram from "../assets/Instagram Icon.png";
+import facebook from "../assets/Facebook Icon.png";
+import github from "../assets/GitHub Icon.png";
+
+
+const Footer = () => (
+  <div>
+    <img src={twitter} />
+    <img src={instagram} />
+    <img src={facebook} />
+    <img src={github} />
+  </div>
+);
+
+export default Footer;
