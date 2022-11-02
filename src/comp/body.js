a3e1fa29512b60867a90d1f8d9b939276042aab2
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-02 11:36:36 +0200

    body content

---
+++ src/comp/body.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Body = () => (
+  <div>
+    <h1>Laura Smith</h1>
+    <h2>Frontend Developer</h2>
+    <div>
+      <button>Email</button>
+      <button>LinkedIn</button>
+    </div>
+    <div>
+      <h1>About</h1>
+      <p>
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+      <h1>Interests</h1>
+      <p>
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  </div>
+);
+
+export default Body
